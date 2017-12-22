f2e553f162541d17b8364584644c9d91cc7d9b9d
neeraj chaudhary <neeraj.jat@gmail.com>
2017-12-22 10:58:01 +0530

    updated requirements.txt and app.py for mongo db.

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb://admin:password@ds157653.mlab.com:57653/ddpm'
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
-    #return "Hello World!"
     return render_template('index.html')
+
+@app.route("/testmongo")
+def testmongo():
+    mongo.db.employees.insert({'abcd':'abcd'})
+    return "Inserted"
 
 if __name__ == "__main__":
     app.run()
