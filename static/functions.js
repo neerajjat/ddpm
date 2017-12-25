b3bc3753229be86c1d5c2fb08c68b0b5ac7b0d9c
neeraj chaudhary <neeraj.jat@gmail.com>
2017-12-25 05:55:38 +0530

    Adding scores and weights for bugs and tests. Computing overall score by multiplying scores and weights for bugs and tests

--- static/functions.js
+++ static/functions.js
@@ -1,7 +1,7 @@
-function load_slider(objects, obj){
-    obj_id = '#' + objects[obj]
+function load_slider(obj){
+    obj_id = '#' + obj
     slider_id = obj_id + '_' + 'slider'
-    console.log('obj_id:' + obj_id + ':' + 'slider_id:' + slider_id)
+    console.log('Load time obj_id:' + obj_id + ':' + 'slider_id:' + slider_id)
     $(slider_id).slider({
     range: "max",
     min: 1,
@@ -9,7 +9,7 @@ function load_slider(objects, obj){
     value: 5,
     slide: function(event, ui){
         $(obj_id).val(ui.value);
-        console.log('obj_id:' + obj_id + '|' + 'slider_value:' + ui.value)
+        console.log('Slider action obj_id:' + obj_id + '|' + 'slider_value:' + ui.value)
     }
     });
     $(obj_id).val( $(slider_id).slider("value"));
